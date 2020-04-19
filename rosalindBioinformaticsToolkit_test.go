71be1b13d9339867ea7d7758fbca3309dad29e60
weetabix <weetabix@weetabix.cx>
2020-04-19 12:33:06 -0600

    Set up unit tests.

--- rosalindBioinformaticsToolkit_test.go
+++ rosalindBioinformaticsToolkit_test.go
@@ -10,16 +10,16 @@ func TestCountDNABases(t *testing.T) {
 		input      string
 		wantResult map[string]int
 	}{
-		{"AGTC", map[string]int{
-			"A": 1,
+		{"AGATCTT", map[string]int{
+			"A": 2,
 			"C": 1,
 			"G": 1,
-			"T": 1,
+			"T": 3,
 		},
 		},
 	}
 	for _, tt := range tests {
-		if gotResult := countDNABases(string(tt.input)); !cmp.Equal(gotResult, tt.wantResult) {
+		if gotResult := countDNABases(tt.input); !cmp.Equal(gotResult, tt.wantResult) {
 			t.Errorf("countDNABases(%v) = %v, want %v", tt.input, gotResult, tt.wantResult)
 		}
 	}
